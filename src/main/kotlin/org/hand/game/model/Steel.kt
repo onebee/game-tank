7ade32cc6269be99af121eed0640ee528cbedc3f
Kaibin <diaokaibin@gmail.com>
2019-03-05 15:07:57 +0800

    移动检测阻挡能力完成

--- src/main/kotlin/org/hand/game/model/Steel.kt
+++ src/main/kotlin/org/hand/game/model/Steel.kt
@@ -1,13 +1,15 @@
 package org.hand.game.model
 
 import org.hand.game.Config
+import org.hand.game.business.Blockable
 import org.itheima.kotlin.game.core.Painter
+
 // 铁墙
-class Steel(override var x: Int, override var y: Int) : View {
+class Steel(override var x: Int, override var y: Int) : View, Blockable {
 
     override var width: Int = Config.block
         set(value) {}
-    override var height: Int  = Config.block
+    override var height: Int = Config.block
         set(value) {}
 
     override fun draw() {
