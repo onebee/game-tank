b194559486c0cc6df90d9ef76a0fae7cb57df830
Kaibin <diaokaibin@gmail.com>
2019-03-05 09:55:33 +0800

    边界处理

--- src/main/kotlin/org/hand/game/model/Tank.kt
+++ src/main/kotlin/org/hand/game/model/Tank.kt
@@ -30,12 +30,23 @@ class Tank(override var x: Int, override var y: Int) : View {
     }
 
     fun move(direction: Direction) {
-        this.currentDirection = direction
+
+        // 当前方向和希望的方向不一致时候才去改变方向
+        if (this.currentDirection != direction) {
+            this.currentDirection = direction
+            return
+        }
         when (direction) {
             Direction.UP -> y -= speed
             Direction.DOWN -> y += speed
             Direction.LEFT -> x -= speed
             Direction.RIGHT -> x += speed
         }
+
+        //边界处理
+        if (x < 0) x = 0
+        if (x > Config.gameWidth - width) x = Config.gameWidth - width
+        if (y < 0) y = 0
+        if (y > Config.gameHeight - height) y = Config.gameHeight - height
     }
 }
